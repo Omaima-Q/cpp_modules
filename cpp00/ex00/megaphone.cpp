709bdc4788f2863898c0bf6953e32155690842f6
Omaima <oqaroot@student.42abudhabi.ae>
2026-04-22 15:18:15 +0400

    Update megaphone.cpp

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -12,20 +12,20 @@
 
 #include <iostream> //the library for input, output operations 
 #include <cctype>   // we use this library for functions that perform character handling like toupper function and etc
-#include <string>
+#include <string>    // The string class is part of the C++ standard library. A string represents a sequence of characters.
 
 int main(int argc, char **argv)
 {
-    if (argc == 1)
+    if (argc == 1) // if you have only the name of the program in the console 
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return (0);
     }
 
-    for (int i = 1; i < argc; i++)
+    for (int i = 1; i < argc; i++) // loop through each string 
     {
         std::string s(argv[i]); // Initialize string from char*
-        for (size_t j = 0; j < s.length(); j++)
+        for (size_t j = 0; j < s.length(); j++) // loop through each character in each string 
         {
             std::cout << (char)std::toupper(s[j]);
         }
